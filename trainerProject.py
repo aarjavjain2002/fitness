673b79ee32ac3c9b3a1cbdee6dfbb3e8002a746e
aarjav <ajain1@unc.edu>
2024-04-03 18:36:44 -0400

    TODO: SSL Certificate Error

--- trainerProject.py
+++ trainerProject.py
@@ -1,12 +1,24 @@
 import cv2
 import numpy as np
 import time
+import poseModule as pm
 
-cap = cv2.VideoCapture(1)   
-#- Reading the image
+# cap = cv2.VideoCapture(0)   
+
+detector = pm.poseDetector()
 
 while True:
-    success, img = cap.read()
+    # #- Reading the image
+    # success, img = cap.read()
+
+    # #- Resizing the image
+    # img = cv2.resize(img, (1280, 720))
+
+    img = cv2.imread("assets/test.jpg")
+
+    img = detector.findPose(img, draw=True)
+
+    #- Displaying the image
     cv2.imshow("Image", img)
-    cv2.waitkey(1)
+    cv2.waitKey(1)
 
